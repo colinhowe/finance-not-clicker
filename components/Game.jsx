45836f1cd6aba2c4e8cda61a436a5ab48c328d4f
Colin Howe <colinthehowe@gmail.com>
2015-07-23 16:45:02 +0000

    senior devs!

--- components/Game.jsx
+++ components/Game.jsx
@@ -4,9 +4,28 @@ import Score from "./Score.jsx"
 
 const TICK_INTERVAL = 1000
 
+class Building {
+  constructor(name, cost) {
+    this.name = name
+    this.cost = cost
+  }
+}
+
+const BUILDINGS = {
+  'juniorDev': new Building('Junior Dev', 100000),
+  'seniorDev': new Building('Senior Dev', 200000),
+}
+
+
 class AddDevButton extends React.Component {
   render() {
-    return <div onClick={this.props.addDev}>Hire dev</div>
+    return <div onClick={this.clickHandler.bind(this)}>
+      Hire {this.props.building.name}
+    </div>
+  }
+
+  clickHandler() {
+    this.props.handler(this.props.buildingKey)
   }
 }
 
@@ -15,14 +34,23 @@ export default class Game extends React.Component {
     super(props);
     this.state = {
       currentLoc: 0,
-      devs: 1,
+      money: 500000,
     };
 
+    let buildings = {}
+    for (let key in BUILDINGS) {
+      buildings[key] = 0
+    }
+
+    this.state.buildings = buildings
     this.state.locPerSecond = this.calculateNewLocSpeed(this.state)
   }
 
   calculateNewLocSpeed(proposedState) {
-    return proposedState.devs * 1
+    return (
+      proposedState.buildings['juniorDev'] * 1
+      + proposedState.buildings['seniorDev'] * 3
+    )
   }
 
   componentDidMount() {
@@ -30,28 +58,47 @@ export default class Game extends React.Component {
   }
 
   render() {
+    let buttons = []
+    for (let key in BUILDINGS) {
+      buttons.push(<AddDevButton
+        buildingKey={key}
+        building={BUILDINGS[key]}
+        handler={this.addBuilding.bind(this)}
+      />)
+    }
     return <div>
       <Score
         currentLoc={this.state.currentLoc}
         locPerSecond={this.state.locPerSecond}
+        devs={this.state.devs}
+        money={this.state.money}
       />
-      <AddDevButton addDev={this.addDev.bind(this)} />
+      {buttons}
     </div>;
   }
 
   tick() {
-    var elapsed_time = TICK_INTERVAL / 1000 // Number of seconds elapsed
+    let elapsed_time = TICK_INTERVAL / 1000 // Number of seconds elapsed
     
-    var locIncrement = this.state.locPerSecond * elapsed_time
-    var newLoc = this.state.currentLoc + locIncrement
+    let locIncrement = this.state.locPerSecond * elapsed_time
+    let newLoc = this.state.currentLoc + locIncrement
 
     this.setState({currentLoc: newLoc})
   }
 
-  addDev() {
-    var proposedState = {
-      devs: this.state.devs + 1,
+  addBuilding(key) {
+    if (this.state.money < BUILDINGS[key].cost) {
+      return
     }
+
+    let proposedState = {
+      money: this.state.money - BUILDINGS[key].cost,
+      buildings: {},
+    }
+    for (let key in BUILDINGS) {
+      proposedState.buildings[key] = this.state.buildings[key]
+    }
+    proposedState.buildings[key] = this.state.buildings[key] + 1
     proposedState.locPerSecond = this.calculateNewLocSpeed(proposedState)
 
     this.setState(proposedState)
--- components/Score.jsx
+++ components/Score.jsx
@@ -3,8 +3,10 @@ import React from "react"
 export default class Score extends React.Component {
   render() {
     return <div id="score">
+      <p>Money: ${this.props.money}</p>
       <p>LOC: { this.props.currentLoc}</p>
       <p>LOC/second: { this.props.locPerSecond}</p>
+      <p>Devs: { this.props.devs}</p>
     </div>;
   }
 }
